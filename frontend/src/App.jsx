86088c511bf6060100ddb8791ca49c0888becc86
Jill munanura <jillmunanura32@gmail.com>
2026-03-28 22:52:37 +0300

    Add register route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './loginPage';
+import RegisterPage from './RegisterPage';
 import ProtectedRoute from './ProtectedRoute';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={
           <ProtectedRoute>
             <h1>Welcome to Dashboard</h1>

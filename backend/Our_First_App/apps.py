aff4b7333f068fb9dc8054d323636cc86a7df8e2
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-04-08 01:25:48 -0700

    modified apps.py

--- backend/Our_First_App/apps.py
+++ backend/Our_First_App/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class OurFirstAppConfig(AppConfig):
-    name = 'Our_First_App'
+class InternshipConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Our_First_App'  

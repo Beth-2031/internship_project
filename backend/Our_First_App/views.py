9c0607d18c36bde0cac871b3a560927200fc412b
Jill munanura <jillmunanura32@gmail.com>
2026-04-12 15:05:47 +0300

    Add InternshipPlacement viewset

--- backend/Our_First_App/views.py
+++ backend/Our_First_App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewset
+from .models import InternshipPlacement
+from .serializers import InternshipPlacementSerializer
+
+class InternshipPlacementPlacementViewSet(viewsets.ModelViewSet):
+    queryset = InternshipPlacement.objects.all()
+    serializer_class = InternshipPlacementSerializer
 
 # Create your views here.

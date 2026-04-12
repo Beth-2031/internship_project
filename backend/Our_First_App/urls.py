b2eee9ee63dcece74867ff8ffb757de986b59716
Jill munanura <jillmunanura32@gmail.com>
2026-04-12 15:25:52 +0300

    Add Placement API urls

--- backend/Our_First_App/urls.py
+++ backend/Our_First_App/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import InternshipPlacementViewSet
+
+router = DefaultRouter()
+router.register(r'placements', InternshipPlacementViewSet)
 
 urlpatterns = [
     path('redirect/', views.redirect_user, name='redirect_user'),
@@ -8,4 +13,6 @@ urlpatterns = [
     path('workplace/', views.workplace_dashboard, name='workplace_dashboard'),
     path('academic/', views.academic_dashboard, name='academic_dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+
+    path('api/', include(router.urls)),
 ]

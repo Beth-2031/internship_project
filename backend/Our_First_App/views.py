055df4f47477d2276cd08229645d55c2bc28e967
Jill munanura <jillmunanura32@gmail.com>
2026-04-12 18:56:54 +0300

    Placement API working

--- backend/Our_First_App/views.py
+++ backend/Our_First_App/views.py
@@ -4,8 +4,25 @@ from .models import InternshipPlacement
 from .serializers import InternshipPlacementSerializer
 
 class InternshipPlacementViewSet(viewsets.ModelViewSet):
-    queryset = InternshipPlacement.object.all()
+    queryset = InternshipPlacement.objects.all()
     serializer_class = InternshipPlacementSerializer
-    
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
+def redirect_user(request):
+    return JsonResponse({'message': 'redirect'})
+
+def student_dashboard(request):
+    return JsonResponse({'message': 'student dashboard'})
+
+def workplace_dashboard(request):
+    return JsonResponse({'message': 'workplace dashboard'})
+
+def academic_dashboard(request):
+    return JsonResponse({'message': 'academic dashboard'})
+
+def admin_dashboard(request):
+    return JsonResponse({'message': 'admin dashboard'})
+
 
 

41b760ec186c989be4b708d5c698bd6ecb4c4e08
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-04-13 04:03:48 -0700

    modified serializers.py

--- backend/Our_First_App/serializers.py
+++ backend/Our_First_App/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 from .models import InternshipPlacement
 
-<<<<<<< HEAD
-class InternshipPlacementSerializer(serializers.ModelSerial):
-    class Meta:
-        model = InternshipPlacement
-        field = '__all__'
-=======
 class InternshipPlacementSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipPlacement
         fields = '__all__'
->>>>>>> Jill

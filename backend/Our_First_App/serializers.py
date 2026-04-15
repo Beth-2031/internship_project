ee3448826edf829cbd9237173cf37f903c865363
Jill munanura <jillmunanura32@gmail.com>
2026-04-15 17:27:09 +0300

    add a lock check

--- backend/Our_First_App/serializers.py
+++ backend/Our_First_App/serializers.py
@@ -5,3 +5,10 @@ class InternshipPlacementSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipPlacement
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        if instance.is_approved:
+            raise serializers.ValidationError(
+                'This placement has been approved and cannot be edited.'
+            )
+        return super().update(instance, validated_data)

295435aeb1ff4151742152578ff6a8991b15a290
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-04-15 07:17:04 -0700

    modified deploymentsettings.py

--- backend/backend_project/wsgi.py
+++ backend/backend_project/wsgi.py
@@ -16,6 +16,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend_project.settings')
 
 application = get_wsgi_application()

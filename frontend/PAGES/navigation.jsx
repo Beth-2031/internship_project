38096571bceea938602a98fd5f1a4fa73594c3bf
Thomas1 <thomasmigadde@gmail.com>
2026-04-15 11:28:30 +0300

    add

--- frontend/PAGES/navigation.jsx
+++ frontend/PAGES/navigation.jsx
@@ -3,4 +3,25 @@ import {useContext, createContext} from 'react';
 const NavigationContext = createContext();
 
 
-export const NavigationProvider = ({ children }) => {}
+export const NavigationProvider = ({ children }) => {
+    const Navigate = useNavigate();
+    const goToHome = () => Navigate('/');
+    const goToStudentDashboard = () => Navigate('/student/dashboard');
+    const goToSupervisorDashboard = () => Navigate('/supervisor/dashboard');
+    const goToAcademicDashboard = () => Navigate('/academic/dashboard');
+    const goToAdminDashboard = () => Navigate('/admin/dashboard');
+}
+
+return (
+    <NavigationContext.Provider value={{ goToHome, goToStudentDashboard, goToSupervisorDashboard, goToAcademicDashboard, goToAdminDashboard }}>
+        {children}
+    </NavigationContext.Provider>
+);
+
+
+export function useNavigation (){
+    const context = useContext(NavigationContext);
+    if (!context) {
+        throw new Error('useNavigation must be used within a NavigationProvider');
+    }
+}

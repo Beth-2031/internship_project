cebf08ed5cf0d90b19f2a1471fd3653e6dc7b9ba
Thomas1 <thomasmigadde@gmail.com>
2026-04-16 09:21:50 +0300

    serializer

--- backend/Our_First_App/serializers.py
+++ backend/Our_First_App/serializers.py
@@ -1,3 +1,20 @@
 from rest_framework import serializers
-from .models import InternshipPlacement
+from .models import InternshipPlacement, CustomUser, WeeklyLog, SafetyReport, CourseCompletion
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'user_type','skills']
+
+
+class InternshipPlacementSerializer(serializers.ModelSerializer):
+    student = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.filter(user_type='student'))
+    workplace_supervisor = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.filter(
+        user_type='workplace_supervisor'), 
+        required=False,
+        allow_null=True)        
+    academic_supervisor = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.filter(user_type='academic_supervisor'),
+        required=False,
+        allow_null=True)

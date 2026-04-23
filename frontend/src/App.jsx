9104e1afe5e8208b24f1e68b04eadba207dea279
Elizabeth <elizabethnabachwa42@gmail.com>
2026-04-23 13:42:06 +0300

    layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,26 @@ import LoginPage from './loginPage';
 import RegisterPage from './RegisterPage';
 import './STYLES/App.css'
 import ProtectedRoute from './ProtectedRoute';
+
+import StudentLayout from './layouts/StudentLayout';
+import SupervisorLayout from './layouts/SupervisorLayout';
+import AcademicLayout from './layouts/AcademicLayout';
+import AdminLayout from './layouts/AdminLayout';
+
 import StudentDashboard from './pages/student/Dashboard';
 import StudentPlacements from './pages/student/Placements';
 import StudentMyPlacement from './pages/student/MyPlacement';
+
 import SupervisorDashboard from './pages/supervisor/Dashboard';
 import SupervisorPlacements from './pages/supervisor/Placements';
 import SupervisorStudents from './pages/supervisor/Students';
+
 import AcademicDashboard from './pages/academic/Dashboard';
 import AcademicPlacements from './pages/academic/Placements';
 import AcademicStudents from './pages/academic/Students';
+
 import AdminDashboard from './pages/admin/Dashboard';
 import AdminPlacements from './pages/admin/AllPlacements';
-
 
 function App() {
   return (
@@ -23,84 +31,41 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/student/dashboard" element={
-          <ProtectedRoute allowedRoles={['student']}>
-            <StudentDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/student" element={
-          <ProtectedRoute allowedRoles={['student']}>
-            <StudentDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/student/placements" element={
-          <ProtectedRoute allowedRoles={['student']}>
-            <StudentPlacements />
-          </ProtectedRoute>
-        } />
-        <Route path="/student/my-placement" element={
-          <ProtectedRoute allowedRoles={['student']}>
-            <StudentMyPlacement />
-          </ProtectedRoute>
-        } />
-         <Route path="/supervisor/dashboard" element={
-          <ProtectedRoute allowedRoles={['workplace_supervisor']}>
-            <SupervisorDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/supervisor" element={
-          <ProtectedRoute allowedRoles={['workplace_supervisor']}>
-            <SupervisorDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/supervisor/placements" element={
-          <ProtectedRoute allowedRoles={['workplace_supervisor']}>
-            <SupervisorPlacements />
-          </ProtectedRoute>
-        } />
-        <Route path="/supervisor/students" element={
-          <ProtectedRoute allowedRoles={['workplace_supervisor']}>
-            <SupervisorStudents />
-          </ProtectedRoute>
-        } />
-        <Route path="/academic/dashboard" element={
-          <ProtectedRoute allowedRoles={['academic_supervisor']}>
-            <AcademicDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/academic" element={
-          <ProtectedRoute allowedRoles={['academic_supervisor']}>
-            <AcademicDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/academic/placements" element={
-          <ProtectedRoute allowedRoles={['academic_supervisor']}>
-            <AcademicPlacements />
-          </ProtectedRoute>
-        } />
-        <Route path="/academic/students" element={
-          <ProtectedRoute allowedRoles={['academic_supervisor']}>
-            <AcademicStudents />
-          </ProtectedRoute>
-        } />
-        <Route path="/admin/dashboard" element={
-          <ProtectedRoute allowedRoles={['internship_admin']}>
-            <AdminDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/admin" element={
-          <ProtectedRoute allowedRoles={['internship_admin']}>
-            <AdminDashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/admin/placements" element={
-          <ProtectedRoute allowedRoles={['internship_admin']}>
-            <AdminPlacements />
-          </ProtectedRoute>
-        } />  
-      </Routes>
 
+        {/* Student routes */}
+        <Route element={<ProtectedRoute allowedRoles={['student']}><StudentLayout /></ProtectedRoute>}>
+          <Route path="/student" element={<StudentDashboard />} />
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/student/placements" element={<StudentPlacements />} />
+          <Route path="/student/my-placement" element={<StudentMyPlacement />} />
+        </Route>
+
+        {/* Workplace Supervisor routes */}
+        <Route element={<ProtectedRoute allowedRoles={['workplace_supervisor']}><SupervisorLayout /></ProtectedRoute>}>
+          <Route path="/supervisor" element={<SupervisorDashboard />} />
+          <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
+          <Route path="/supervisor/placements" element={<SupervisorPlacements />} />
+          <Route path="/supervisor/students" element={<SupervisorStudents />} />
+        </Route>
+
+        {/* Academic Supervisor routes */}
+        <Route element={<ProtectedRoute allowedRoles={['academic_supervisor']}><AcademicLayout /></ProtectedRoute>}>
+          <Route path="/academic" element={<AcademicDashboard />} />
+          <Route path="/academic/dashboard" element={<AcademicDashboard />} />
+          <Route path="/academic/placements" element={<AcademicPlacements />} />
+          <Route path="/academic/students" element={<AcademicStudents />} />
+        </Route>
+
+        {/* Admin routes */}
+        <Route element={<ProtectedRoute allowedRoles={['internship_admin']}><AdminLayout /></ProtectedRoute>}>
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/placements" element={<AdminPlacements />} />
+        </Route>
+
+      </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;

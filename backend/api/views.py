fabfc9f8a8a5fc02e38a394f71a7c9c7442ae241
Jill munanura <jillmunanura32@gmail.com>
2026-04-24 00:13:20 +0300

    Fixing CSRF in view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response

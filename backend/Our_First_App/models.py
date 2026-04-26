fbc673186010f14bfd56bf086408e86440594cb2
Thomas1 <thomasmigadde@gmail.com>
2026-04-26 19:42:40 +0300

    models for notifications

--- backend/Our_First_App/models.py
+++ backend/Our_First_App/models.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
 
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+   
 
 
 

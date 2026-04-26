904b46583868bc5f9cb2a1b72aa1eb21855c9c17
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-04-26 18:52:53 +0300

    modified views.py

--- backend/Our_First_App/urls.py
+++ backend/Our_First_App/urls.py
@@ -1,35 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import InternshipPlacementViewSet , WeeklyLogViewSet, SafetyReportViewSet, CourseCompletionViewSet
-from django.views.decorators.csrf import csrf_exempt
-from api.views import UserViewSet
-from .views import login_view
-
+from .views import InternshipPlacementViewSet
+from .views import register_view, login_view
 # DRF Router
 router = DefaultRouter()
 router.register(r'placements', InternshipPlacementViewSet, basename='placements')
-router.register(r'weekly-logs', WeeklyLogViewSet, basename='weekly-logs')
-router.register(r'safety-reports', SafetyReportViewSet, basename='safety-reports')
-router.register(r'course-completions', CourseCompletionViewSet, basename='course-completions')
-router.register(r'users', UserViewSet, basename='users') 
 
 urlpatterns = [
     # ===========================
     # DASHBOARDS
-    # ===========================
+    path('api/register/', register_view),
+    path('api/login/', login_view),
     path('', views.dashboard, name='dashboard'),
     path('student/', views.student_dashboard, name='student_dashboard'),
     path('workplace/', views.workplace_dashboard, name='workplace_dashboard'),
     path('academic/', views.academic_dashboard, name='academic_dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-     path('api/login/', login_view, name='login'),
 
     # ===========================
     # API
     # ===========================
     path('api/', include(router.urls)),
-
-    path('log/<int:log_id>/edit/', views.edit_weekly_log, name='edit_weekly_log'),
-    path('placement/<int:placement_id>/edit/', views.edit_placement, name='edit_placement'),
 ]

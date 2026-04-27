f23e921fde331a3a9501670c6f33ed3dd3f9e676
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-04-27 15:49:12 +0300

    modified urls.py in backend

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+qx#l6w-olg)41pe%)6o_#hxz8(xp$0v%bha*!lkhn0qvaoc$)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+qx#l6w-olg)41pe%)6o_#hxz8(xp$0v%bha*!lkhn0qvaoc$)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -153,5 +154,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "thomasmigadde@gmail.com"
-EMAIL_HOST_PASSWORD = "Migadde25901"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'thomasmigadde@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'thomasmigadde@gmail.com')

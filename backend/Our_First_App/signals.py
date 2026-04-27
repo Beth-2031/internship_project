53ae1a861d42ecd10ffab9d5b750cd53c9265221
Thomas1 <thomasmigadde@gmail.com>
2026-04-27 08:30:17 +0300

    signals.py

---
+++ backend/Our_First_App/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import  Notification
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+@receiver(post_save, sender= Notification)
+def send_notification_email(sender, instance, created, **kwargs):
+    if created:
+        subject = "New Notification"
+        message = f"You have a new notification: {instance.message}"
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [instance.user.email]
+
+        send_mail(subject, message, from_email, recipient_list)

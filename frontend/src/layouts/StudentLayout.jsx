331c505f651152abaece2ea242a26a75d2ecee47
Thomas1 <thomasmigadde@gmail.com>
2026-04-29 10:41:47 +0300

    in studentlayout added toastcontainer

--- frontend/src/layouts/StudentLayout.jsx
+++ frontend/src/layouts/StudentLayout.jsx
@@ -2,17 +2,21 @@
 import AppLayout from '../components/layout/AppLayout'
 import { useFetch } from '../hooks/useFetch'
 import { getSafetyReports, getWeeklyLogs } from '../api/client'
+import ToastContainer from '../components/ToastContainer'
 
 export function StudentLayout() {
   const { data: logs }   = useFetch(getWeeklyLogs)
   const { data: safety } = useFetch(getSafetyReports)
   const pendingLogs = logs?.filter(l => !l.is_verified).length ?? 0
   return (
+    <>
     <AppLayout
       badges={{ pendingLogs }}
       topbarTitle="Internship Management System"
       topbarSub="Student Portal"
     />
+    <ToastContainer />
+    </>
   )
 }
 export default StudentLayout

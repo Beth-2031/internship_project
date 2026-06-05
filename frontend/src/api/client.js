1b80ffad4fc8aa29adf0168eb26fc7c67dd6e921
Beth-2031 <elizabethnabachwa42@gmail.com>
2026-06-05 08:49:37 +0300

    interceptor request

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -7,6 +7,17 @@ const api = axios.create({
   xsrfCookieName: 'csrftoken',
   xsrfHeaderName: 'X-CSRFToken',
 })
+api.interceptors.request.use(config => {
+  const csrfToken = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('csrftoken='))
+    ?.split('=')[1]
+  
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken
+  }
+  return config
+})
 api.interceptors.response.use(
   res => res,
   err => {
